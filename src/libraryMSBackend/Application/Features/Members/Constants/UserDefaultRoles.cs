8d58b03fc06392f4b60b31916b389d0c4232db9d
Sercan Ateş <sercanio@users.noreply.github.com>
2024-05-10 16:42:50 +0300

    Update User Roles

--- src/libraryMSBackend/Application/Features/Members/Constants/UserDefaultRoles.cs
+++ src/libraryMSBackend/Application/Features/Members/Constants/UserDefaultRoles.cs
@@ -1,5 +1,5 @@
 ﻿namespace Application.Features.Members.Constants;
 public static class UserDefaultRoles
 {
-    public static readonly string[] Roles = ["Announcements.Read", "Auth.RevokeToken", "Users.Read", "Members.Read", "MemberSettings.Update", "BookReservations.Create", "BookReservations.Read", "BookReservations.Update", "FavoriteBooks.Create", "FavoriteBooks.Read", "FavoriteBooks.Delete", "Magazines.Read", "EBooks.Read"];
+    public static readonly string[] Roles = ["Announcements.Read", "Auth.RevokeToken", "Users.Read", "Members.Read", "MemberSettings.Update", "MemberSettings.Read", "MemberSettings.Create", "BookReservations.Create", "BookReservations.Read", "BookReservations.Update", "FavoriteBooks.Create", "FavoriteBooks.Read", "FavoriteBooks.Delete", "Magazines.Read", "EBooks.Read"];
 }
